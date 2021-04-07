a69fe74722cc706f705abdddd8569073c8569c30
hileamlak <hilea.buisness@gmail.com>
2021-04-07 10:29:16 +0000

    Add task 100

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,6 +1,12 @@
-# Addes some configration to the ssh_config file
+# Sets up your client SSH configuration file to use a private key
+include stdlib
 
-exec { 'ssh_config':
-  command => 'echo -e "\n"'
+file_line { 'USE private key':
+  path  =>  '/etc/ssh/ssh_config',
+  line  =>  'IdentityFile ~/.ssh/holberton',
+}
 
+file_line { 'TURN OFF Password Auth':
+  path  =>  '/etc/ssh/ssh_config',
+  line  =>  'PasswordAuthentication no',
 }
